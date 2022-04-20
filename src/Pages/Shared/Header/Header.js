e1fd849cf4a14da321c25de116766b4dd7b5cd78
unknown <mstuhin>
2022-04-20 15:23:21 +0600

    header

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -36,9 +36,7 @@ const Header = () => {
                                 <button className='btn btn-link text-white text-decoration-none' onClick={handleSignOut}>sing out</button>
 
                                 :
-                                <Nav.Link as={Link} to="login">
-                                    Login
-                                </Nav.Link>
+                                
                         }
                     </Nav>
                 </Navbar.Collapse>
