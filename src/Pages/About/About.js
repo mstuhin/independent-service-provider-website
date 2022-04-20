0c5ed7e8304c9a30d488ab5de47834d91eab3761
unknown <mstuhin>
2022-04-20 13:34:59 +0600

    trainer

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,25 @@
 import React from 'react';
-
+import trainer from '../../images/Trainer.png';
 const About = () => {
     return (
-        <div>
-            <h2>this about us</h2>
+        <div className='d-flex'>
+            <div className='mx-5 mt-5'>
+                <img src={trainer} alt="" />
+            </div>
+            <div className='mt-5'>
+                <h1>Josh Mage</h1>
+                <p>Personal Trainer</p>
+                <h3 className='text-primary'>Biography:</h3>
+                <p>Joshua has been working in the Health, Fitness,
+                    <br /> and Sports industry for over 10 years in different capacities as
+                    <br /> a Group Fitness Instructor, Personal Trainer, and Wellness and Fitness Consultant.
+                    <br /> He is now delivering Personal Training, Boxercise,
+                    <br /> and Group Fitness Exercises to all age groups bringing with him all the experience in Nutrition, Fitness,
+                    <br /> Weight Management and Rehabilitation Exercises to help his clients achieve their goals.
+
+                </p>
+
+            </div>
         </div>
     );
 };
--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import { Link, useParams } from 'react-router-dom';
+import Service from '../Home/Service/Service';
 
 const ServiceDetail = () => {
     const { serviceId } = useParams();
     return (
         <div>
             <h2>welcome to detail:{serviceId}</h2>
+
+
             <div className='text-center'>
+
                 <Link to="/checkout">
                     <button className='btn btn-primary'>Proceed Checkout</button>
                 </Link>
